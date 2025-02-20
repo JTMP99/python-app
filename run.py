aa298b50b3ed420b6f51ac47783016d4a4bd0efd
JTMP99 <james.tm.phillips@gmail.com>
2025-02-20 13:58:43 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
+
+@app.route('/')
+def healthcheck():
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.getenv('PORT', 8080)))
