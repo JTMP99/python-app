ce562d7218d736a10ed1e7be9c8a876add30f62d
JTMP99 <james.tm.phillips@gmail.com>
2025-02-22 15:52:53 -0500

    Update 23

--- app/streaming/capture.py
+++ app/streaming/capture.py
@@ -72,7 +72,7 @@ class StreamCapture:
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
-            chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
+            chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN', '/usr/bin/chromium')
 
             self.driver = webdriver.Chrome(options=chrome_options)
             logging.info("Selenium WebDriver initialized successfully")
